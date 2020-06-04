f7e6460296a99813a97cfe770fffed5ea9aabdac
saadhassan <saad.haxxan786@gmail.com>
2020-06-04 17:54:20 +0500

    Updates

---
+++ precache-manifest.84f7e5aefce0c0978835c8d25696d555.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e03a111b7b22e907b70285bd8769f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9697223827e722da874",
+    "url": "/static/css/main.d1ccf352.chunk.css"
+  },
+  {
+    "revision": "8d78cd0b744595d49c11",
+    "url": "/static/js/2.8b8c604b.chunk.js"
+  },
+  {
+    "revision": "b9697223827e722da874",
+    "url": "/static/js/main.a80f292a.chunk.js"
+  },
+  {
+    "revision": "a595a062f32bbdc150c2",
+    "url": "/static/js/runtime-main.21aed1dd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3db91d488ac4d7e570e36e7529018f19",
+    "url": "/static/media/JP-Morgan-Chase-logo.3db91d48.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "bb63ebb35e68a00aeef89961d781d4b3",
+    "url": "/static/media/virufyLogo.bb63ebb3.png"
+  }
+]);
